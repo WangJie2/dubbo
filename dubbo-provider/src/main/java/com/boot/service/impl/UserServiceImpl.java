61f57b43e6aed7612be8899f28dce49c35db2312
王杰 <1054918167@qq.com>
2018-07-10 18:29:10 +0800

    bug修改

--- dubbo-provider/src/main/java/com/boot/service/impl/UserServiceImpl.java
+++ dubbo-provider/src/main/java/com/boot/service/impl/UserServiceImpl.java
@@ -15,6 +15,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public String sayHello(String str) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        System.out.println("===============hello===============");
         return dateFormat.format(new Date()) + ": " + str;
     }
 
@@ -26,6 +27,7 @@ public class UserServiceImpl implements UserService {
         userDto.setAge(28);
         userDto.setId(1001);
         userDto.setGender(0);
+        System.out.println("===============user===============");
         return userDto;
     }
 }
